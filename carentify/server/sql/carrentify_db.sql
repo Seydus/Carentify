5b0a0805f90b8f15acab2be98332486927c5706e
Seydus <weljohncatanpatan@gmail.com>
2023-12-13 13:16:41 +0800

    added sql file

--- carentify/server/sql/carrentify_db.sql
+++ carentify/server/sql/carrentify_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2023 at 02:16 AM
+-- Generation Time: Dec 13, 2023 at 06:15 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,9 +28,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `Admin_ID` int(11) NOT NULL,
-  `Emp_ID` int(11) DEFAULT NULL
+  `Admin_ID` int(20) NOT NULL,
+  `Emp_ID` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`Admin_ID`, `Emp_ID`) VALUES
+(1, 1),
+(2, 3);
 
 -- --------------------------------------------------------
 
@@ -39,8 +47,16 @@ CREATE TABLE `admin` (
 --
 
 CREATE TABLE `cash_payments` (
-  `Cash_Order_ID` int(11) NOT NULL
+  `Cash_Order_ID` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cash_payments`
+--
+
+INSERT INTO `cash_payments` (`Cash_Order_ID`) VALUES
+(1),
+(2);
 
 -- --------------------------------------------------------
 
@@ -49,8 +65,19 @@ CREATE TABLE `cash_payments` (
 --
 
 CREATE TABLE `company` (
-  `Company` int(11) NOT NULL
+  `Company` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`Company`) VALUES
+(1),
+(2),
+(3),
+(4),
+(5);
 
 -- --------------------------------------------------------
 
@@ -64,8 +91,19 @@ CREATE TABLE `customer` (
   `Last_Name` varchar(255) DEFAULT NULL,
   `Contact_no` varchar(20) DEFAULT NULL,
   `Payment_Method` varchar(255) DEFAULT NULL,
-  `No_Passengers` int(11) DEFAULT NULL
+  `email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`Rental_No`, `First_Name`, `Last_Name`, `Contact_no`, `Payment_Method`, `email`) VALUES
+(101, 'Juan', 'Dela Cruz', '+639171234567', 'Online Payment', 'juan@gmail.com'),
+(102, 'Maria', 'Santos', '+639181234567', 'Cash Payment', NULL),
+(103, 'Jose', 'Lopez', '+639191234567', 'Online Payment', NULL),
+(104, 'Rosa', 'Gonzalez', '+639201234567', 'Cash Payment', NULL),
+(105, 'Carlos', 'Reyes', '+639211234567', 'Online Payment', NULL);
 
 -- --------------------------------------------------------
 
@@ -74,9 +112,17 @@ CREATE TABLE `customer` (
 --
 
 CREATE TABLE `driver` (
-  `License_no` int(11) NOT NULL,
-  `Emp_ID` int(11) DEFAULT NULL
+  `License_no` int(20) NOT NULL,
+  `Emp_ID` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `driver`
+--
+
+INSERT INTO `driver` (`License_no`, `Emp_ID`) VALUES
+(101, 2),
+(102, 4);
 
 -- --------------------------------------------------------
 
@@ -85,10 +131,37 @@ CREATE TABLE `driver` (
 --
 
 CREATE TABLE `employee` (
-  `Emp_ID` int(11) NOT NULL,
+  `Emp_ID` int(20) NOT NULL,
   `First_Name` varchar(255) DEFAULT NULL,
   `Last_Name` varchar(255) DEFAULT NULL,
   `Position` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`Emp_ID`, `First_Name`, `Last_Name`, `Position`) VALUES
+(1, 'John', 'Doe', 'Admin'),
+(2, 'Jane', 'Smith', 'Driver'),
+(3, 'Bob', 'Johnson', 'Admin'),
+(4, 'Alice', 'Jones', 'Driver'),
+(5, 'Charlie', 'Brown', 'Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location_details`
+--
+
+CREATE TABLE `location_details` (
+  `id` int(11) NOT NULL,
+  `pickUpLocation` varchar(255) NOT NULL,
+  `dropOffLocation` varchar(255) NOT NULL,
+  `pickUpDate` date NOT NULL,
+  `dropOffDate` date NOT NULL,
+  `pickUpTime` time NOT NULL,
+  `dropOffTime` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -98,9 +171,17 @@ CREATE TABLE `employee` (
 --
 
 CREATE TABLE `online_payments` (
-  `Online_Order_ID` int(11) NOT NULL,
-  `Account_Number` int(11) DEFAULT NULL
+  `Online_Order_ID` int(20) NOT NULL,
+  `Account_Number` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `online_payments`
+--
+
+INSERT INTO `online_payments` (`Online_Order_ID`, `Account_Number`) VALUES
+(1, 987654321),
+(3, 654321987);
 
 -- --------------------------------------------------------
 
@@ -109,9 +190,20 @@ CREATE TABLE `online_payments` (
 --
 
 CREATE TABLE `order_receipt` (
-  `Receipt_ID` int(11) NOT NULL,
-  `Order_ID` int(11) DEFAULT NULL
+  `Receipt_ID` int(20) NOT NULL,
+  `Order_ID` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_receipt`
+--
+
+INSERT INTO `order_receipt` (`Receipt_ID`, `Order_ID`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5);
 
 -- --------------------------------------------------------
 
@@ -120,10 +212,21 @@ CREATE TABLE `order_receipt` (
 --
 
 CREATE TABLE `package` (
-  `Package_ID` int(11) NOT NULL,
-  `Car_ID` int(11) DEFAULT NULL,
-  `Rental_No` int(11) DEFAULT NULL
+  `Package_ID` int(20) NOT NULL,
+  `Car_ID` int(20) DEFAULT NULL,
+  `Rental_No` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `package`
+--
+
+INSERT INTO `package` (`Package_ID`, `Car_ID`, `Rental_No`) VALUES
+(1, 1, 101),
+(2, 2, 102),
+(3, 1, 103),
+(4, 2, 104),
+(5, 1, 105);
 
 -- --------------------------------------------------------
 
@@ -132,34 +235,25 @@ CREATE TABLE `package` (
 --
 
 CREATE TABLE `payment_method` (
-  `Order_ID` int(11) NOT NULL,
-  `Rental_No` int(11) DEFAULT NULL,
-  `Day` int(11) DEFAULT NULL,
-  `Month` int(11) DEFAULT NULL,
-  `Year` int(11) DEFAULT NULL,
+  `Order_ID` int(20) NOT NULL,
+  `Rental_No` int(20) DEFAULT NULL,
+  `Day` int(20) DEFAULT NULL,
+  `Month` int(20) DEFAULT NULL,
+  `Year` int(20) DEFAULT NULL,
   `Type` varchar(255) DEFAULT NULL,
-  `Reference_No` int(11) DEFAULT NULL
+  `Reference_No` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `sedan`
+-- Dumping data for table `payment_method`
 --
 
-CREATE TABLE `sedan` (
-  `Plate_no` char(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `van`
---
-
-CREATE TABLE `van` (
-  `Plate_no` char(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `payment_method` (`Order_ID`, `Rental_No`, `Day`, `Month`, `Year`, `Type`, `Reference_No`) VALUES
+(1, 101, 8, 12, 2023, 'Online Payment', 123456),
+(2, 102, 8, 12, 2023, 'Cash Payment', 789012),
+(3, 103, 8, 12, 2023, 'Online Payment', 345678),
+(4, 104, 8, 12, 2023, 'Cash Payment', 901234),
+(5, 105, 8, 12, 2023, 'Online Payment', 567890);
 
 -- --------------------------------------------------------
 
@@ -168,11 +262,23 @@ CREATE TABLE `van` (
 --
 
 CREATE TABLE `vehicle` (
-  `Car_ID` int(11) NOT NULL,
+  `Car_ID` int(20) NOT NULL,
   `Plate_no` char(10) DEFAULT NULL,
-  `License_no` int(11) DEFAULT NULL,
-  `Seat_Capacity` int(11) DEFAULT NULL
+  `License_no` int(20) DEFAULT NULL,
+  `Seat_Capacity` int(20) DEFAULT NULL,
+  `vehicleMake` varchar(255) DEFAULT NULL,
+  `vehicleType` varchar(255) DEFAULT NULL,
+  `transmission` varchar(255) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`Car_ID`, `Plate_no`, `License_no`, `Seat_Capacity`, `vehicleMake`, `vehicleType`, `transmission`, `price`) VALUES
+(1, 'ABC123', 101, 4, 'Hyundai', 'Sedan', 'Manual', 8371),
+(2, 'XYZ456', 102, 6, 'Toyota', 'Van', 'Automatic', 13561);
 
 --
 -- Indexes for dumped tables
@@ -217,6 +323,12 @@ ALTER TABLE `employee`
   ADD PRIMARY KEY (`Emp_ID`);
 
 --
+-- Indexes for table `location_details`
+--
+ALTER TABLE `location_details`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `online_payments`
 --
 ALTER TABLE `online_payments`
@@ -245,18 +357,6 @@ ALTER TABLE `payment_method`
   ADD KEY `Rental_No` (`Rental_No`);
 
 --
--- Indexes for table `sedan`
---
-ALTER TABLE `sedan`
-  ADD PRIMARY KEY (`Plate_no`);
-
---
--- Indexes for table `van`
---
-ALTER TABLE `van`
-  ADD PRIMARY KEY (`Plate_no`);
-
---
 -- Indexes for table `vehicle`
 --
 ALTER TABLE `vehicle`
@@ -269,64 +369,16 @@ ALTER TABLE `vehicle`
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `location_details`
 --
-ALTER TABLE `admin`
-  MODIFY `Admin_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `cash_payments`
---
-ALTER TABLE `cash_payments`
-  MODIFY `Cash_Order_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `company`
---
-ALTER TABLE `company`
-  MODIFY `Company` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `Rental_No` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `Emp_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `online_payments`
---
-ALTER TABLE `online_payments`
-  MODIFY `Online_Order_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `order_receipt`
---
-ALTER TABLE `order_receipt`
-  MODIFY `Receipt_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `package`
---
-ALTER TABLE `package`
-  MODIFY `Package_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `payment_method`
---
-ALTER TABLE `payment_method`
-  MODIFY `Order_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `location_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `vehicle`
 --
 ALTER TABLE `vehicle`
-  MODIFY `Car_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Car_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -367,9 +419,7 @@ ALTER TABLE `payment_method`
 -- Constraints for table `vehicle`
 --
 ALTER TABLE `vehicle`
-  ADD CONSTRAINT `vehicle_ibfk_3` FOREIGN KEY (`License_no`) REFERENCES `driver` (`License_no`),
-  ADD CONSTRAINT `vehicle_ibfk_4` FOREIGN KEY (`Plate_no`) REFERENCES `sedan` (`Plate_no`) ON DELETE CASCADE,
-  ADD CONSTRAINT `vehicle_ibfk_5` FOREIGN KEY (`Plate_no`) REFERENCES `van` (`Plate_no`) ON DELETE CASCADE;
+  ADD CONSTRAINT `vehicle_ibfk_3` FOREIGN KEY (`License_no`) REFERENCES `driver` (`License_no`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
